3bfe579c8efd2ab92359797ac88a5f86f08c8745
fn ⌃ ⌥ <70830482+FnControlOption@users.noreply.github.com>
2024-07-17 10:51:49 -0700

    spec: `as` without parentheses

--- spec/compiler/parser/parser_spec.cr
+++ spec/compiler/parser/parser_spec.cr
@@ -2419,6 +2419,7 @@ module Crystal
       assert_end_location %(asm("nop"))
       assert_end_location "1+2"
       assert_end_location "1-2"
+      assert_end_location "foo.as Bar"
 
       assert_syntax_error %({"a" : 1}), "space not allowed between named argument name and ':'"
       assert_syntax_error %({"a": 1, "b" : 2}), "space not allowed between named argument name and ':'"
